e6b4ebf2140051643f800eafb6e5d5abc0eac188
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-31 01:02:51 +0530

    Add files via upload

---
+++ assignment 3.12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+ char name[50];
+ int n;
+ printf("Enter your name:");
+ scanf("%s", name);
+ for(int i = 0; i <= n; i++)
+ {
+  printf("%s\n", name);
+ }
+ return 0;
+}
