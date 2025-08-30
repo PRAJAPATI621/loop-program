a42a4519eb6f66e418d01cb56d0f1b0825be0b4a
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-31 00:16:01 +0530

    Update assignment 3.7.c

--- assignment 3.7.c
+++ assignment 3.7.c
@@ -6,9 +6,10 @@ int main()
  printf("Enter a positive integers (n):");
  scanf("%d", &n);
  for(i = 1; i <= n; i++)
- sum = n*n;
+ sum = n*(n+1)/2;
  {
   printf("sum of first %d natural number: %f\n", n,sum);
  }
  return 0;
 }
+
