a00aa3cacfeeefc66c665e24ba6a9a303e2dbab0
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-09-04 22:56:04 +0530

    Add files via upload

---
+++ assignment 3.13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int sum = 0;
+    for(i=1; i<=100; i++)
+    {
+    //Check if the cuurrent number is divisible by 13
+    if(i % 13 == 0)
+     {
+    sum += i; //Add the number to the sum
+     }
+    }
+    //Print the final sum
+    printf("The sum of all numbers divisible by 13 in the range of 1 to 100 is: %d\n", sum);
+     return 0;
+}
